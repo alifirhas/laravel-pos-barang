d60744c201a5369c609e9efd9c15019cad423990
alifirhas <irhasalif@gmail.com>
2023-06-15 14:01:27 +0700

    :sparkles: Guest lihat barang

--- app/Http/Controllers/Guest/BarangController.php
+++ app/Http/Controllers/Guest/BarangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Barang;
 use Illuminate\Http\Request;
 
 class BarangController extends Controller
@@ -12,6 +13,10 @@ class BarangController extends Controller
      */
     public function index()
     {
-        return view('guest.dashboard');
+        $barangs = Barang::paginate(10); // Menampilkan 10 produk per halaman
+
+        return view('guest.dashboard', [
+            'barangs' => $barangs,
+        ]);
     }
 }
--- resources/views/guest/dashboard.blade.php
+++ resources/views/guest/dashboard.blade.php
@@ -3,7 +3,55 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    You are guest
+                    <!-- Tampilkan data barang -->
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Nama Barang
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        barcode
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Harga satuan
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Stok
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($barangs as $barang)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <td></td>
+                                        <td class="px-6 py-4">
+                                            {{ $barang->nama_barang }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $barang->barcode }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            @convertIDR($barang->harga_satuan)
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $barang->stok }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+
+                    <!-- Tampilkan link navigasi paginasi -->
+                    <div class="mt-4">
+                        {{ $barangs->links() }}
+                    </div>
                 </div>
             </div>
         </div>
